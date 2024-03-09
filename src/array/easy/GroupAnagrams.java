7eeab6f1523c9a959cfafc870f27724844432ec5
Aykhan Mammadli <mmmdliayxan2107@gmail.com>
2024-03-09 19:58:51 +0400

    groupanagram problem is solved

---
+++ src/array/easy/GroupAnagrams.java
@@ -0,0 +1,32 @@
+package array.easy;
+
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> doubleList = new ArrayList<>();
+        Map<String,List<String>> map = new HashMap<>();
+        for(String value : strs){
+           char[] chrArr = value.toCharArray();
+           Arrays.sort(chrArr);
+           String sortedWord= Arrays.toString(chrArr);
+            if(!map.containsKey(sortedWord)){
+                List<String> valueList= new ArrayList<>();
+                valueList.add(value);
+                map.put(sortedWord,valueList);
+            }
+            else{
+                map.get(sortedWord).add(value);
+            }
+        }
+
+        for(String key : map.keySet()){
+            doubleList.add(map.get(key));
+        }
+
+
+        return doubleList;
+
+    }
+}
