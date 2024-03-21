fe5283f463781e9be65ed1f00f0d818d4d019c2f
Aykhan Mammadli <mmmdliayxan2107@gmail.com>
2024-03-21 20:36:37 +0400

    polindrom number is solved

---
+++ src/array/easy/PolindromNumber.java
@@ -0,0 +1,23 @@
+package array.easy;
+
+public class PolindromNumber {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+
+        int[] array = new int[100];
+        int t=0;
+        while(x!=0){
+            array[t++]=x%10;
+            x=x/10;
+        }
+        for(int i=0;i<t/2;i++){
+            if(array[i]!=array[t-i-1]){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
